f65b4f69b33b226ec9815767fa153ccba9983194
Deepanshu Tripathi <deepanshutr@gmail.com>
2023-04-27 14:20:40 +0530

    feat(data): GetBondWeight() return type to sdkTypes.Int

--- go/data/constants/data_weight.go
+++ go/data/constants/data_weight.go
@@ -1,12 +1,14 @@
 package constants
 
+import sdkTypes "github.com/cosmos/cosmos-sdk/types"
+
 var (
-	AccAddressDataWeight = int64(90)
-	BooleanDataWeight    = int64(1)
-	DecDataWeight        = int64(16)
-	HeightDataWeight     = int64(8)
-	IDDataWeight         = int64(64)
-	ListDataWeight       = int64(1024)
-	NumberDataWeight     = int64(8)
-	StringDataWeight     = int64(256)
+	AccAddressDataWeight = sdkTypes.NewInt(90)
+	BooleanDataWeight    = sdkTypes.NewInt(1)
+	DecDataWeight        = sdkTypes.NewInt(16)
+	HeightDataWeight     = sdkTypes.NewInt(8)
+	IDDataWeight         = sdkTypes.NewInt(64)
+	ListDataWeight       = sdkTypes.NewInt(1024)
+	NumberDataWeight     = sdkTypes.NewInt(8)
+	StringDataWeight     = sdkTypes.NewInt(256)
 )
