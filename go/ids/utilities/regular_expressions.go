b3abbfc040fe40d30ebf4eee7edcd64ce12908ea
deepanshutr <deepanshutr@gmail.com>
2023-05-23 18:54:46 +0530

    fix(stringID): zero length string ID should pass validate basic

--- go/ids/utilities/regular_expressions.go
+++ go/ids/utilities/regular_expressions.go
@@ -2,4 +2,4 @@ package utilities
 
 import "regexp"
 
-var IsValidStringID = regexp.MustCompile(`[A-Za-z0-9_]{1,250}$`).MatchString
+var IsValidStringID = regexp.MustCompile(`[A-Za-z0-9_]{0,250}$`).MatchString
