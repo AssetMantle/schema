a00ce592597ee81882ab6be7206b69e885a758bb
deepanshutr <deepanshutr@gmail.com>
2023-08-15 15:57:09 +0530

    refactor(data): moving regular_expressions from utilities to constants

---
+++ go/data/constants/regular_expressions.go
@@ -0,0 +1,6 @@
+package constants
+
+import "regexp"
+
+var IsValidStringData = regexp.MustCompile(`^[\pL\pM\pZ\pS\pN\pP]{0,280}$`).MatchString
+var IsValidFileExtension = regexp.MustCompile(`(?:jpg|jpeg|png|gif|bmp|svg|mp4|avi|mov|wmv|flv|mp3|wav|ogg|aac)$`).MatchString
