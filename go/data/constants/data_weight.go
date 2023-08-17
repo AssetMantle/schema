1489903730971b449a47817d6872ab542c484007
deepanshutr <deepanshutr@gmail.com>
2023-08-17 09:28:14 +0530

    feat(ListData): readjusting amortised weight

--- go/data/constants/data_weight.go
+++ go/data/constants/data_weight.go
@@ -8,7 +8,7 @@ var (
 	DecDataWeight        = sdkTypes.NewInt(16)
 	HeightDataWeight     = sdkTypes.NewInt(8)
 	IDDataWeight         = sdkTypes.NewInt(64)
-	ListDataWeight       = sdkTypes.NewInt(1024)
+	ListDataWeight       = sdkTypes.NewInt(2560)
 	LinkedDataWeight     = sdkTypes.NewInt(384)
 	NumberDataWeight     = sdkTypes.NewInt(8)
 	StringDataWeight     = sdkTypes.NewInt(256)
