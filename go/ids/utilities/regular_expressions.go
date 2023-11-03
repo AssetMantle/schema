4f1db270c4faf721555bbe5c0ed9c7be5432d2d9
Deepanshu Tripathi <deepanshutr@gmail.com>
2023-11-03 09:46:47 +0530

    fix(ids):string ID regex

--- go/ids/utilities/regular_expressions.go
+++ go/ids/utilities/regular_expressions.go
@@ -2,4 +2,4 @@ package utilities
 
 import "regexp"
 
-var IsValidStringID = regexp.MustCompile(`[A-Za-z0-9_]{0,30}$`).MatchString
+var IsValidStringID = regexp.MustCompile(`^[A-Za-z0-9_]{0,30}$`).MatchString
