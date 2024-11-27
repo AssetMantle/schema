15a0b3a7f6cfcd245de82070eedd44f4dd2b02a9
deepanshutr <deepanshutr@gmail.com>
2024-11-27 14:37:49 +0530

    refactor(parameterList): making parameter list default sorted

--- lists/base/parameter_list.go
+++ lists/base/parameter_list.go
@@ -1,28 +1,108 @@
 package base
 
 import (
+	"fmt"
+	"github.com/AssetMantle/schema/ids"
 	"github.com/AssetMantle/schema/lists"
 	"github.com/AssetMantle/schema/parameters"
-	"github.com/AssetMantle/schema/parameters/base"
+	baseParameters "github.com/AssetMantle/schema/parameters/base"
+	"sort"
 )
 
 var _ lists.ParameterList = (*ParameterList)(nil)
 
+func (parameterList *ParameterList) ValidateBasic() error {
+	parameterList.sort()
+
+	parameterIDMap := map[string]bool{}
+
+	for i, parameter := range parameterList.Get() {
+		if parameter == nil {
+			return fmt.Errorf("nil parameter in list at index %d", i)
+		}
+
+		if _, found := parameterIDMap[parameter.GetMetaProperty().GetID().AsString()]; found {
+			return fmt.Errorf("duplicate parameter with ID: %s", parameter.GetMetaProperty().GetID().AsString())
+		} else {
+			parameterIDMap[parameter.GetMetaProperty().GetID().AsString()] = true
+		}
+
+		if err := parameter.ValidateBasic(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 func (parameterList *ParameterList) Get() []parameters.Parameter {
+	parameterList.sort()
+
 	Parameters := make([]parameters.Parameter, len(parameterList.Parameters))
 	for i, parameter := range parameterList.Parameters {
 		Parameters[i] = parameter
 	}
+
 	return Parameters
+}
+func (parameterList *ParameterList) GetParameter(propertyID ids.PropertyID) parameters.Parameter {
+	if index, found := parameterList.search(baseParameters.NewEmptyParameterFromID(propertyID)); found {
+		return parameterList.Parameters[index]
+	}
+
+	return nil
+}
+func (parameterList *ParameterList) Mutate(parameters ...parameters.Parameter) lists.ParameterList {
+	for _, property := range parameters {
+		if property != nil {
+			if index, found := parameterList.search(property); found {
+				parameterList.Parameters[index] = parameterList.Parameters[index].Mutate(property.GetMetaProperty().GetData()).(*baseParameters.Parameter)
+			}
+		}
+	}
+
+	return parameterList
+}
+func (parameterList *ParameterList) sort() lists.ParameterList {
+	sort.Slice(parameterList.Parameters, func(i, j int) bool {
+		return parameterList.Parameters[i].Compare(parameterList.Parameters[j]) < 0
+	})
+	return parameterList
+}
+func (parameterList *ParameterList) search(parameter parameters.Parameter) (index int, found bool) {
+	parameterList.sort()
+
+	if parameter == nil {
+		return -1, false
+	}
+
+	index = sort.Search(
+		len(parameterList.Parameters),
+		func(i int) bool {
+			return parameterList.Parameters[i].Compare(parameter) >= 0
+		},
+	)
+
+	if index < len(parameterList.Parameters) && parameterList.Parameters[index].Compare(parameter) == 0 {
+		return index, true
+	}
+
+	return index, false
+}
+func (parameterList *ParameterList) add(parameters ...parameters.Parameter) lists.ParameterList {
+	for _, parameter := range parameters {
+		if parameter != nil {
+			if index, found := parameterList.search(parameter); !found {
+				parameterList.Parameters = append(parameterList.Parameters, parameter.(*baseParameters.Parameter))
+				copy(parameterList.Parameters[index+1:], parameterList.Parameters[index:])
+				parameterList.Parameters[index] = parameter.(*baseParameters.Parameter)
+			} else {
+				parameterList.Parameters[index] = parameter.(*baseParameters.Parameter)
+			}
+		}
+	}
+	return parameterList
 }
 
 func NewParameterList(parameters ...parameters.Parameter) lists.ParameterList {
-	newParameters := make([]*base.Parameter, len(parameters))
-	for i, parameter := range parameters {
-		newParameters[i] = parameter.(*base.Parameter)
-	}
-
-	return &ParameterList{
-		Parameters: newParameters,
-	}
+	return (&ParameterList{}).add(parameters...)
 }
--- lists/parameter_list.go
+++ lists/parameter_list.go
@@ -1,11 +1,16 @@
 package lists
 
 import (
+	"github.com/AssetMantle/schema/ids"
 	"github.com/AssetMantle/schema/parameters"
-	"github.com/cosmos/gogoproto/proto"
+	"github.com/cosmos/cosmos-sdk/codec"
 )
 
 type ParameterList interface {
-	proto.Message
+	ValidateBasic() error
 	Get() []parameters.Parameter
+	GetParameter(ids.PropertyID) parameters.Parameter
+	Mutate(...parameters.Parameter) ParameterList
+
+	codec.ProtoMarshaler
 }
